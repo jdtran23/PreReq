ad01a293d2d2a3cb690e5ababac9669027fe54d3
emilytran <emilyt@outlook.com>
2013-04-22 18:39:39 -0500

    update to CSS

--- views/displayskill.php
+++ views/displayskill.php
@@ -3,50 +3,97 @@ include("navbar.php");
 $topic = rawurldecode($data['topic']);
 ?>
 
-<div ALIGN=CENTER> 
-<h3> You are now learning the fine art of <font color='blue'><?php echo $topic; ?></font>.</h3><br>
-</div>
+<div class="row">
+		<br>
+			
 
-<div ALIGN=LEFT>
-<a href = <?php echo 'index.php?displaySkill&topic='.rawurlencode($topic).'&fav=1'; ?> class="btn btn-primary">Add to Favorites</a>
+
+	<div class="span2"></div>
+	
+	<div class="span7">
+		<br>
+
+			<h3>You are now learning the fine art of:<br> <a href="#"><?php echo $topic; ?> <button class="btn btn-fav pull-right"><a  href = <?php echo 'index.php?displaySkill&topic='.rawurlencode($topic).'&fav=1'; ?>
+			<i id = "favorite-button" ><i class='icon-star'>
+			</i> Favorite</a></button>
+			</a></h3><span>
+			
+			<div class="wiki-preview btn">
+		
+		HTML
+			
+HyperText Markup Language (HTML) is the main markup language for creating web pages and other information that can be displayed in a web browser.
+HTML is written in the form of HTML elements consisting of tags enclosed in angle brackets, within the web page content. HTML tags most commonly come in pairs like h1, although some tags, known as empty elements, are unpaired, for example <img>. The first tag in a pair is the start tag, the second tag is the end tag (they are also called opening tags and closing tags). In between these tags web designers can add text, tags, comments and other types of text-based content.
+The purpose of a web browser is to read HTML documents and compose them into visible or audible web pages. The browser does not display the HTML tags, but uses the tags to interpret the content of the page.
+HTML elements form the building blocks of all websites. HTML allows images and objects to be embedded and can be used to create interactive forms. It provides a means to create structured documents by denoting structural semantics for text such as headings, paragraphs, lists, links, quotes and other items. It can embed scripts written in languages such as JavaScript which affect the behavior of HTML web pages.
+Web browsers can also refer to Cascading Style Sheets (CSS) to define the appearance and layout of text and other material. The W3C, maintainer of both the HTML and the CSS standards, encourages the use of CSS over explicit presentational HTML markup.[1]
+		
+		</div>
+		<!--<iframe src="http://en.wikipedia.org/wiki/Ashton_Kutcher"></iframe>-->
+		<?php
+			//"<iframe width='700' height='700' src='http://www.en.wikipedia.org/wiki/". $topic ."'></iframe>"
+			?>
+	</div>
+
+	<div class="span4">
+		<br>
+
+
+
  
-  <?php 
-    if(isset($data['add_fav_flag']))
-      if($data['add_fav_flag'])
-        echo "Added to favorites!";
-      else
-        echo "Add failed!"; ?>
+	<?php 
+	if(isset($data['add_fav_flag']))
+	  if($data['add_fav_flag'])
+		echo "<br><div id='success-box' class='alert alert-success'><strong>Success! Added to favorites!</strong></div><script>hideSuccess();</script>";
+	  else
+		echo "<br><div class='alert alert-error'><strong>Oops!</strong> Add failed, perhaps you have already added this skill to your favorites</div>";
+	?>
 
-<br>
-<span class='prereq-item'> PREREQS ARE:<br></span>
-<?php
-    if(isset($data['results_obj']))
-    {
-        $result = $data['results_obj'];
-        if($result->num_rows > 0)
-        {
-          while($row = mysqli_fetch_array($result))
-          {
-          echo "<t><span class='prereq-item'><i class='icon-arrow-right'></i>SKILL: <a href='index.php?displaySkill&topic=".$row[2]."&newprereq=0&prereq=''>".$row[2]."</a></span> <font color='green'><i class='icon-thumbs-up'></i> Up Vote </font><font color='red'><i class='icon-thumbs-down'></i> Down Vote</font>";//." URL: ".$row
-          echo "<t><br/>";
-          }
-        }
-        else
-          echo "<br><h4>There are currently no prereqs! Add some!</h4>";
-  }
-?>
+	<br> 
+
+		<h5><span class='prereq-item'> Prerequisite Skills:<br></span></h5>
+		
+		<?php
+			if(isset($data['results_obj']))
+			{
+				$result = $data['results_obj'];
+				if($result->num_rows > 0)
+				{
+				  while($row = mysqli_fetch_array($result))
+				  {
+					  echo "<t><span class='prereq-item'><i class='icon-arrow-right'></i>&nbsp;<a href='index.php?displaySkill&topic=".$row[2]."&newprereq=0&prereq=''>".$row[2]."
+					  </a></span>
+					<i class='icon-thumbs-up'> 
+					  </i><strong>" . " " . $row[4] . " <a href='upvote-btn'></strong><font color='green'>Upvotes" . " " . "</font></a>
+					  <i class='icon-thumbs-down'>
+					  </i><strong>" . " " . $row[5] . " <a href href='downvote-btn'></strong><font color='red'>Downvotes</font></a>";//." URL: ".$row 
+					  echo "<t><br/>"; 
+				  }
+				}
+				else
+				  echo "<br><div class='alert alert-info'><strong>Wait! </strong>There are currently no prereqs. Add some!</div>";
+		  }
+		?>
+		</br>
+		Suggest a PreReq!
+		<form NAME="searchform" class="form-search">
+			<input NAME="typehere" id="searchterm" /*class='search_input'*/ class="input-large search-query" value="Type here..."onfocus="if(this.value==this.defaultValue)this.value='';" onblur="if(this.value=='')this.value=this.defaultValue;"/> 
+		</form> 
+			
+		<div id="results"></div>	
+		
+		<h5>Learning Resources</h5>
+		<form action="/html/codes/html-comment-box-code-action.cfm" method="get">
+
+		<textarea rows="4" cols="100"  placeholder="Share Learning Resources Here!"></textarea><br />
+		<input class="btn" type="submit" value="Submit" />
+		</form>
+
+	</div>
+
+	
+
 </div>
-
-
-<div ALIGN = CENTER>
-<font size = 3>Suggest a PreReq!</font>
-<form NAME="searchform" class="form-search">
-<input NAME="typehere" id="searchterm" /*class='search_input'*/ class="input-large search-query" value="Type here..."onfocus="if(this.value==this.defaultValue)this.value='';" onblur="if(this.value=='')this.value=this.defaultValue;"/> 
-</form> 
-</div>
-
-<div id="results"></div>
-
 </body>
 <script>
   $("#searchterm").keyup(function(e){ //modified 
@@ -63,7 +110,7 @@ $topic = rawurldecode($data['topic']);
     $("#results").empty(); 
     $.each(data.query.search, function(i,item){
       console.log(item.title);
-    $("#results").append("<div>"+"<a href='index.php?displaySkill&topic="+"<?php echo $topic; ?>"+"&newprereq=1&prereq="+escape(item.title)+"'''>Learn it!</a>" + item.title + "</a><br>" + item.snippet +"</div>"); 
+    $("#results").append("<div>"+"<a href='index.php?displaySkill&topic="+"<?php echo $topic; ?>"+"&newprereq=1&prereq="+escape(item.title)+"'''>Prereq It! </a>" + item.title + "</a><br>" + item.snippet +"</div>"); 
     });
   });
   });
--- views/skillsearch.php
+++ views/skillsearch.php
@@ -3,24 +3,25 @@
 
 <body onLoad="document.forms.searchform.typehere.focus()">
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
-<ul class="breadcrumb">
-  <li><a href="#">Home</a> <span class="divider">/</span></li>
-  <li><a href="#">Search</a> <span class="divider">/</span></li>
-  <li class="active">Query1</li>s
-</ul>
 
-<div>
 
-<div ALIGN=CENTER> 
-<div><font size = 10>Start typing in a skill here...</font> </div> <!-- prereq.css is not linking, temp-->
-<form NAME="searchform" class="form-search">
-<input NAME="typehere" id="searchterm" /*class='search_input'*/ class="input-large search-query" value="Type here..."onfocus="if(this.value==this.defaultValue)this.value='';" onblur="if(this.value=='')this.value=this.defaultValue;"/> 
-</form> 
-</div>
+	<div class="row">
+	<div class="span5"></div>
+	<div class="span7">
+		<div ALIGN=CENTER> 
+		<div><font size = 10>Start typing in a skill here...</font> </div> <!-- prereq.css is not linking, temp-->
+		<form NAME="searchform" class="form-search">
+		<input NAME="typehere" id="searchterm" /*class='search_input'*/ class="input-large search-query" value="Type here..."onfocus="if(this.value==this.defaultValue)this.value='';" onblur="if(this.value=='')this.value=this.defaultValue;"/> 
+		</form> 
+		</div>
 
-<div id="results"></div>
-
+		<div id="results"></div>
+	</div>
+	</div>
 </body>
+
+
+
 <script>
   $("#searchterm").keyup(function(e){ //modified 
 	var q = $("#searchterm").val();
@@ -36,7 +37,7 @@
 	  $("#results").empty(); 
 	  $.each(data.query.search, function(i,item){
 			console.log(item.title);
-		$("#results").append("<div>"+"<a href='index.php?displaySkill&topic="+item.title.replace('&','%26')+"&newprereq=0&prereq='''>Learn it!</a>" + item.title + "</a><br>" + item.snippet +"</div>"); 
+		$("#results").append("<div>"+"<strong>" + item.title + "</strong></a>"  + "<a class='pull-right' href='index.php?displaySkill&topic="+item.title.replace('&','%26')+"&newprereq=0&prereq='''>Learn it!</a> <br>"+ item.snippet +"<hr SIZE='4'></div>")  ; 
 	  });
 	});
   });
